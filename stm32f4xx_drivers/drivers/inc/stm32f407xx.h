94019b29fbf810b41db5331e66d6bce0aede0823
Urmil Premal Shah <51014316+urmilshah1@users.noreply.github.com>
2022-10-12 14:39:12 -0400

    Add files via upload

--- stm32f4xx_drivers/drivers/inc/stm32f407xx.h
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx.h
@@ -400,6 +400,55 @@ typedef struct {
 #define GPIO_PIN_RESET 	RESET
 
 
+/**************************************************************************
+ * Bit position definitions for SPI peripherals
+ **************************************************************************/
+/*
+ * Bit position definitions SPI_CR1
+ */
+#define SPI_CR1_CPHA 		0
+#define SPI_CR1_CPOL 		1
+#define SPI_CR1_MSTR		2
+#define SPI_CR1_BR			3
+#define SPI_CR1_SPE			6
+#define SPI_CR1_LSBFIRST	7
+#define SPI_CR1_SSI			8
+#define SPI_CR1_SSM			9
+#define SPI_CR1_RXONLY		10
+#define SPI_CR1_DFF			11
+#define SPI_CR1_CRCNEXT		12
+#define SPI_CR1_CRCEN		13
+#define SPI_CR1_BIDIODE		14
+#define SPI_CR1_BIDIMODE	15
+
+/*
+ * Bit position definitions SPI_CR2
+ */
+
+#define SPI_CR2_RXMAEN 		0
+#define SPI_CR2_TXMAEN 		1
+#define SPI_CR2_SSOE		2
+#define SPI_CR2_FRF			4
+#define SPI_CR2_ERRIE		5
+#define SPI_CR2_RXNEIE		6
+#define SPI_CR2_TXEIE		7
+
+
+/*
+ * Bit position definitions SPI_SR
+ */
+
+#define SPI_SR_RXNE			0
+#define SPI_SR_TXE			1
+#define SPI_SR_CHSIDE		2
+#define SPI_SR_UDR			3
+#define SPI_SR_CRC_ERR		4
+#define SPI_SR_MODF			5
+#define SPI_SR_OVR			6
+#define SPI_SR_BSY			7
+#define SPI_SR_FRE			8
+
+
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
 
